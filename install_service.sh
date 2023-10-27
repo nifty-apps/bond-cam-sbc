153f62b64aa6c2b1d855a4852762e3f94265405f
ahmad-ml <ahmad@niftyitsolution.com>
2023-10-27 17:22:39 +0600

    update: services

--- install_service.sh
+++ install_service.sh
@@ -3,10 +3,15 @@ sudo apt install -y python3-pip git
 pip3 install python-dotenv
 mkdir /home/nifty/Projects
 cd /home/nifty/Projects
-git clone git@github.com:nifty-apps/bond-cam-sbc.git /home/nifty/Projects/bondcam_streaming
+git clone https://bond-cam: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@github.com/nifty-apps/bond-cam-sbc.git /home/nifty/Projects/bondcam_streaming
 mkdir -p /home/nifty/Projects/bondcam_streaming/videos/
 chown -R nifty /home/nifty/Projects/bondcam_streaming
 cd /home/nifty/Projects/bondcam_streaming
 sudo cp bondcam_streaming.service /etc/systemd/system/
 sudo systemctl enable bondcam_streaming
 sudo systemctl start bondcam_streaming
+
+sudo cp bondcam_startup.service /etc/systemd/system/
+sudo systemctl enable bondcam_startup.service
+sudo systemctl start bondcam_startup.service
+
