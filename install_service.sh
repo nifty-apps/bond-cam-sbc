a0c615d5cc7ba9f40e1116f28bec536c82f960d0
Pavel Zolotykh <pzolotykh@gmail.com>
2023-10-27 22:40:01 +0530

    .env file renaming to setup script

--- install_service.sh
+++ install_service.sh
@@ -7,11 +7,10 @@ git clone https://bond-cam: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@github.com/n
 mkdir -p /home/nifty/Projects/bondcam_streaming/videos/
 chown -R nifty /home/nifty/Projects/bondcam_streaming
 cd /home/nifty/Projects/bondcam_streaming
+mv .env.default .env
 sudo cp bondcam_streaming.service /etc/systemd/system/
 sudo systemctl enable bondcam_streaming
 sudo systemctl start bondcam_streaming
-
 sudo cp bondcam_startup.service /etc/systemd/system/
 sudo systemctl enable bondcam_startup.service
 sudo systemctl start bondcam_startup.service
-
