a1358735696c1b7e79fb2c3858a8efbb66fe9c19
ahmad-ml <ahmad@niftyitsolution.com>
2023-11-08 18:52:38 +0600

    fix: service files are now made shortcut rather than copy

--- install_service.sh
+++ install_service.sh
@@ -3,13 +3,13 @@ sudo apt install -y python3-pip git
 pip3 install python-dotenv
 mkdir /home/nifty/Projects
 cd /home/nifty/Projects
-git clone https://bond-cam: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@github.com/nifty-apps/bond-cam-sbc.git /home/nifty/Projects/bondcam_streaming
+git clone https://bond-cam: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@github.com/nifty-apps/bond-cam-sbc.git /home/nifty/Projects/bondcam_streaming
 chown -R nifty /home/nifty/Projects/bondcam_streaming
 cd /home/nifty/Projects/bondcam_streaming
 mv .env.default .env
-sudo cp bondcam_streaming.service /etc/systemd/system/
+sudo ln -s /home/nifty/Projects/bondcam_streaming/bondcam_streaming.service /etc/systemd/system/bondcam_streaming.service
 sudo systemctl enable bondcam_streaming
 sudo systemctl start bondcam_streaming
-sudo cp bondcam_startup.service /etc/systemd/system/
+sudo ln -s /home/nifty/Projects/bondcam_streaming/bondcam_startup.service /etc/systemd/system/bondcam_startup.service
 sudo systemctl enable bondcam_startup.service
 sudo systemctl start bondcam_startup.service
